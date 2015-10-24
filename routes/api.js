2152dc8e93355ac08bd148af636703bfebacac9d
Brian Pinsard <brian.pinsard.u8248@student.hogent.be>
2015-10-24 15:10:12 +0200

    - Custom route added for getting all challenges of a specific category

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,23 @@ var Category = require('../models/category'),
 // Routes
 //      Categories
 Category.methods(['get', 'put', 'post', 'delete']);
+
+//      All tasks of a specific user: /categories/id/challenges
+Category.route('challenges', {
+    detail: true,
+    handler: function(req, res, next) {
+        Challenge.find({ category : req.params.id })
+            //populate('category') will fill our category data
+            .populate('category')
+            .exec( function(err, challenges){
+                if(err) return next(err);
+                if(!challenges) return next(new Error('Challenge with ' + req.params.id + ' is null.'));
+
+                res.send(challenges)
+            });
+    }
+});
+
 Category.register(router, '/categories');
 
 //      Challenges
@@ -23,8 +40,7 @@ User.methods(['get', 'put', 'post', 'delete']);
 User.route('tasks', {
     detail: true,
     handler: function(req, res, next) {
-        //populate('tasks.challenge') will fill our challenge data within tasks,
-        //without populate we would return the objectId
+        //populate('tasks.challenge') will fill our challenge data within tasks
         User.findOne({ _id : req.params.id })       // api/users/req.params.id/tasks
             .populate('tasks.challenge')
             .exec( function(err, user){
