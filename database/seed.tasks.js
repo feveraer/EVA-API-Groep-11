fa67c1c3590337ca75f9a6e7df8b34fe83a92717
Tjen Wellens <tjen@Tjens-MacBook-Pro.local>
2015-11-11 15:40:53 +0100

    extracted seed.tasks in seperate module

---
+++ database/seed.tasks.js
@@ -0,0 +1,74 @@
+/*global require,console,module*/
+var config = {
+  DAY_IN_MS: 86400000,
+  TASKS_PER_USER: 30,
+  TASKS_PER_DAY: 3,
+  DAY_OFFSET: 5
+};
+
+function generateTasks (challenges) {
+  var tasks = [];
+  for (var dayIndex = 0; dayIndex < config.TASKS_PER_USER; dayIndex++) {
+    tasks = tasks.concat(
+      generateTasksForDay(challenges, dayIndex)
+    );
+  }
+  return tasks;
+}
+
+function generateTasksForDay (challenges, dayIndex) {
+  var tasks = [];
+  var usedCategories = [];
+  for (var taskPerDayIndex = 0; taskPerDayIndex < config.TASKS_PER_DAY; taskPerDayIndex++) {
+    var challenge = findChallenge(challenges, dayIndex, taskPerDayIndex, usedCategories);
+    var status = getStatusPerDayIndex(dayIndex, taskPerDayIndex);
+    var task = {
+      dueDate: generateDate(dayIndex),
+      challenge: challenge._id,
+      completed: (dayIndex < 3),                                          //TODO: replace.. For timeline development!
+      status: status                                     // previous days = 2, current day = 1, tomorrow = 0
+    };
+    // unique category -> add to tasks, next iteration
+    usedCategories.push(challenge.category);
+    tasks.push(task);
+  }
+  usedCategories = [];
+  return tasks;
+}
+
+//first task's date is 5 days before today,
+//add a day to taskDate per task
+function generateDate (taskIndex) {
+  var taskDate = new Date();
+  var daysToSubstract = config.DAY_OFFSET * config.DAY_IN_MS;
+  var daysToAdd = (taskIndex + 1) * config.DAY_IN_MS;
+
+  taskDate.setTime(taskDate.getTime() - daysToSubstract + daysToAdd);
+  return taskDate;
+}
+
+// not random, keep looping
+function findChallenge (challenges, dayIndex, taskPerDayIndex, categoriesUsedToday) {
+  var challengeIndex = dayIndex * config.TASKS_PER_DAY + taskPerDayIndex;
+  // start at the beginning if more requested than there are challenges
+  challengeIndex %= challenges.length;
+  return challenges[challengeIndex];
+}
+
+// give status for day
+function getStatusPerDayIndex (dayIndex, taskPerDayIndex) {
+  if (taskPerDayIndex > 0) {
+    return 0; // only one task per day can have a status != 0
+  }
+
+  var today = config.DAY_OFFSET - 1;
+  if (dayIndex < today) { // past
+    return 2; // COMPLETED
+  } else if (dayIndex === today) { // today & future
+    return 0; // NONE
+  }
+}
+
+module.exports = {
+  generateTasks: generateTasks
+};
